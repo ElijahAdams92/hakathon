3d296df474180b9fa9ce4ef782b18d3512d9e895
Jabre Thornton <Jabre@Jabres-MacBook-Pro.local>
2016-08-18 09:42:51 -0400

    Adding intent handler factory

--- demoApp/src/js/app.js
+++ demoApp/src/js/app.js
@@ -26,7 +26,8 @@ angular.module('app', ['ngMaterial', 'ngRoute'])
         .primaryPalette('red')
         .accentPalette('grey');
     })
-    .controller('AppCtrl', ['$scope', '$mdSidenav','luisService', function($scope, $mdSidenav,luisService) {
+    .controller('AppCtrl', ['$scope', '$mdSidenav','luisService','intentService',
+                            function($scope, $mdSidenav,luisService,intentService) {
         $scope.chatInput = '';
         $scope.userChats = [];
         $scope.botChats = ['Hello.  How can I help you?'];
@@ -44,25 +45,63 @@ angular.module('app', ['ngMaterial', 'ngRoute'])
 
         $scope.currentNavItem = 'home';
 
-        $scope.openLuis = luisService.callIntent();
+        luisService.callIntent('Tell me about careers',intentService.determineIntent);
     }]).factory('luisService',['$http',function($http){
 
-        var LUIS_URL = 'https://api.projectoxford.ai/luis/v1/application?id=c30e33ac-81ba-487b-99a5-e318f955226f&subscription-key=74722d25371d43f9b2d0d9d72eae583e&q=careers',
-            getIntents = function(){
+
+        var LUIS_URL = 'https://api.projectoxford.ai/luis/v1/application?id=c30e33ac-81ba-487b-99a5-e318f955226f&subscription-key=74722d25371d43f9b2d0d9d72eae583e&q=',
+            getIntents = function(query,callback){
 
                 $http({
                     method: 'GET',
-                    url: LUIS_URL
+                    url: LUIS_URL+query
                 }).
                 success(function(status) {
                     var json=JSON.stringify(status);
-
-                    alert(json);
                     json=JSON.parse(json);
+
+                    callback(json);
                 }).
                 error(function(status) {
                     //your code when fails
                 });
             };
             return {"callIntent":getIntents};
-    }]);
+    }]).factory('intentService',function(){
+            var determineIntent = function(jsonObject){
+                if(jsonObject)
+                {
+                        var firstIntent = jsonObject.intents[0],
+                            foundIntent,
+                            returnIntent = '';
+
+                        if(firstIntent)
+                        {
+                               foundIntent  = firstIntent.intent;
+
+                                switch(foundIntent)
+                                {
+                                    case 'Careers':
+                                        returnIntent = 'Careers';
+                                        break;
+                                    case 'Account':
+                                        returnIntent = 'Account';
+                                        break;
+                                    case 'Balance':
+                                        returnIntent = 'Balance';
+                                        break;
+                                    case 'Contact':
+                                        returnIntent = 'Contact';
+                                        break;
+                                    default:
+                                        returnIntent = 'Home';
+                                        break;
+                                }
+                        }
+                    alert('Hi I found the intent '+foundIntent);
+                    return returnIntent;
+                }
+            };
+
+            return {"determineIntent":determineIntent};
+    });
