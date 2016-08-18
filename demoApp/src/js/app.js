14ea8800c373da7b67e568ca111d6b3f33bb1e8b
Jabre Thornton <Jabre@Jabres-MacBook-Pro.local>
2016-08-18 14:44:58 -0400

    Can search fund list by ticker to retrieve basic fund info.

--- demoApp/src/js/app.js
+++ demoApp/src/js/app.js
@@ -18,16 +18,19 @@ angular.module('app', ['ngMaterial', 'ngRoute', 'ngResource','apiFactories', 'be
             }).
             otherwise('/home');
     }])
-    .config(function($httpProvider){
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    })
+    .config(['$httpProvider', function($httpProvider) {
+            $httpProvider.defaults.useXDomain = true;
+            delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        }
+    ])
     .config(function($mdThemingProvider) {
       $mdThemingProvider.theme('default')
         .primaryPalette('red')
         .accentPalette('grey');
     })
-    .controller('AppCtrl', ['$scope', '$mdSidenav','luisService','intentService','newsRecentStories','newsPersonalfinance','newsEconWeek','jobs',
-                            function($scope, $mdSidenav,luisService,intentService, newsRecentStories, newsPersonalfinance, newsEconWeek, jobs) {
+    .controller('AppCtrl', ['$scope', '$mdSidenav','luisService','intentService','newsRecentStories','newsPersonalfinance',
+                            'newsEconWeek','jobs','getFundInfoService','findFundService',
+                            function($scope, $mdSidenav,luisService,intentService, newsRecentStories, newsPersonalfinance, newsEconWeek, jobs,getFundInfoService,findFundService) {
         $scope.chatInput = '';
         $scope.userChats = [];
         $scope.botChats = ['Hello.  How can I help you?'];
@@ -56,6 +59,17 @@ angular.module('app', ['ngMaterial', 'ngRoute', 'ngResource','apiFactories', 'be
         };
 
         luisService.getIntent('Tell me about careers',intentService.determineIntent);
+
+        getFundInfoService.getFunds($scope);
+
+        $scope.findFundByTicker =  function(fundTicker){
+            return findFundService.findFundsByTicker($scope.funds,fundTicker);
+        }
+
+        $scope.testFunction = function (){
+            var fund = $scope.findFundByTicker('VFINX');
+        };
+
     }]).factory('luisService',['$http',function($http){
 
 
